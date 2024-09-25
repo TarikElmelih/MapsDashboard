bf3b9afc60b20562d4aee397811878933ffcc771
TarikElmelih <elmelihtarik@gmail.com>
2024-09-25 22:11:12 +0300

    layout editing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="{{ asset('build/assets/app.DpglDToU.css') }}">
-        <script src="{{ asset('build/assets/app.DI6-W-r-') }}" defer></script>
+        <script src="{{ asset('build/assets/app.DI6-W-r-.js') }}" defer></script>
 
         <!-- Scripts -->
         @vite('resources/css/app.css')
