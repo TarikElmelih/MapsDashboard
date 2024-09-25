11cb8633259e28183e696ebc16177070d49fec34
TarikElmelih <elmelihtarik@gmail.com>
2024-09-25 22:37:39 +0300

    successfuly worked!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('build/assets/app.DpglDToU.css') }}">
-        <script src="{{ asset('build/assets/app.DI6-W-r-.js') }}" defer></script>
+       
 
         <!-- Scripts -->
         @vite('resources/css/app.css')
