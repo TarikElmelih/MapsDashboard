40e3f7eaade252a1ce7a2c504f524fdb2bf21266
TarikElmelih <elmelihtarik@gmail.com>
2024-10-02 16:46:05 +0300

    teacher points

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/trainer/dashboard', [TrainerController::class, 'dashboard'])->name('trainer.dashboard');
         Route::get('/trainer/users/{id}/edit-points', [TrainerController::class, 'editPoints'])->name('trainer.editPoints');
         Route::put('/trainer/users/{id}/update-points', [TrainerController::class, 'updatePoints'])->name('trainer.updatePoints');
-        Route::get('/trainer/points', [TrainerController::class, 'points'])->name('trainer.points');
+        Route::get('/admin/points', [TrainerController::class, 'points'])->name('admin.points');
         Route::get('/admin/users', [AdminController::class, 'index'])->name('admin.users');
 
     });
