23084c61d157b3c425ae6cfdf7db5043c779eb9f
TarikElmelih <elmelihtarik@gmail.com>
2024-10-02 16:46:58 +0300

    calling userfiles model

--- app/Http/Controllers/Trainer/TrainerController.php
+++ app/Http/Controllers/Trainer/TrainerController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Trainer;
 
 use App\Http\Controllers\Controller;
-
 use App\Models\User;
+use App\Models\UserFile;
 use Illuminate\Http\Request;
 
 class TrainerController extends Controller
